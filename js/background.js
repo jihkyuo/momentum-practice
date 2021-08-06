f7c7a6be81cea8bf3f7a00f3c8a373195eadbd10
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2021-08-07 04:10:53 +0900

    img ++

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
 
 // images Array 안에 Element들을 검색할때 images[0 ~ 2]로 하는데, 랜덤한 수를 주기 위하여 Math.floor(1.8) => 1 반내림 함수 + Math.random() 랜덤 수에 * Array길이
 
